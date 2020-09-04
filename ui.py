4fd7ab21238d6cf72a0922e333cc629a51addfeb
Sree Harsha Nelaturu <nelaturu.harsha@gmail.com>
2020-09-04 14:19:14 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import copy
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
@@ -7,11 +8,13 @@ im_file = st.file_uploader("Choose an image", type=["jpg","png", "jpeg"])
 
 if im_file is not None:
     im = Image.open(im_file)
+    # in place will be replaced by SR'ed image
+    second_im = copy.deepcopy(im)
     st.image(im, caption="Input Image", use_column_width=True)
     
     ## SR
 
-    st.image(im, caption="Output Image", use_column_width=True)
+    st.image(second_im, caption="Output Image", use_column_width=True)
     
     button = st.button("Download")
     if button:
